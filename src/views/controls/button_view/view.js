5d031dd1926c286f0dcabb8c6237c278adf18c8d
Victor Lingenthal <victor.lingenthal@gmail.com>
2013-03-28 22:19:53 +0800

    Update

--- src/views/controls/button_view/view.js
+++ src/views/controls/button_view/view.js
@@ -1,10 +1,12 @@
 BetaJS.Views.ButtonView = BetaJS.Views.View.extend("ButtonView", {
 	_templates: {
-		"default": BetaJS.Templates.Cached["button-view-template"]
+		"default": BetaJS.Templates.Cached["button-view-template"],
+		"icon": BetaJS.Templates.Cached["icon-button-view-template"]
 	},
 	constructor: function(options) {
 		this._inherited(BetaJS.Views.ButtonView, "constructor", options);
 		this._setOptionProperty(options, "label", "");
+		this._setOptionProperty(options, "icon", "");
 	},
 	_events: function () {
 		return this._inherited(BetaJS.Views.ButtonView, "_events").concat([{
