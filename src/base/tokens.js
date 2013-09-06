bb77f8ed7f80ffd24917d41066ecb6c70f65e041
Oliver Friedmann <oliverfriedmann@Oliver-Friedmanns-MacBook-Air.local>
2013-09-06 14:45:23 -0400

    Token Update

--- src/base/tokens.js
+++ src/base/tokens.js
@@ -1,7 +1,11 @@
 BetaJS.Tokens = {
 	
-	generate_token: function () {
-		return Math.random().toString(36).substr(2); 
+	generate_token: function (length) {
+		length = length || 16;
+		var s = "";
+		while (s.length < length)
+			s += Math.random().toString(36).substr(2); 
+		return s.substr(0, length);
 	}
 	
 }
