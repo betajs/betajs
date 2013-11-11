a3ba1077a8387a4e336639dfc1675f042474ca62
Oliver Friedmann <oliverfriedmann@oliverfmannsair.myhome.westell.com>
2013-11-10 21:52:27 -0500

    Dom Update

--- dist/beta.js
+++ dist/beta.js
@@ -5541,45 +5541,103 @@ BetaJS.Browser.AbstractAjax.extend("BetaJS.Browser.JQueryAjax", {
 
 BetaJS.Browser.Dom = {
 	
-	parentElementBySelection : function() {
+	traverseNext: function (node, skip_children) {
+		if ("get" in node)
+			node = node.get(0);
+		if (node.firstChild && !skip_children)
+			return BetaJS.$(node.firstChild);
+		if (!node.parentNode)
+			return null;
+		if (node.nextSibling)
+			return BetaJS.$(node.nextSibling);
+		return this.traverseNext(node.parentNode, true);
+	},
+	
+	selectNode : function(node, offset) {
+		var selection = null;
+		var range = null;			
 		if (window.getSelection) {
-			var target = window.getSelection().getRangeAt(0).commonAncestorContainer;
-			return target.nodeType === 1 ? target : target.parentNode;
-		} else if (document.selection)
-			return document.selection.createRange().parentElement();
+			selection = window.getSelection();
+			selection.removeAllRanges();
+			range = document.createRange();
+		} else if (document.selection) {
+			selection = document.selection;
+			range = selection.createRange();
+		}
+		if (offset) {
+			range.setStart(node, offset);
+			range.setEnd(node, offset);
+			selection.addRange(range);
+		} else {
+			range.selectNode(node);
+			selection.addRange(range);
+		}
+	},
+
+	selectionStartNode : function() {
+		if (window.getSelection)
+			return BetaJS.$(window.getSelection().getRangeAt(0).startContainer);
+		else if (document.selection)
+			return BetaJS.$(document.selection.createRange().startContainer);
+		return null;
+	},
+	
+	selectedHtml : function() {
+		if (window.getSelection)
+			return window.getSelection().toString();
+		else if (document.selection)
+			return document.selection.createRange().htmlText;
+	},
+	
+	selectionAncestor : function() {
+		if (window.getSelection)
+			return BetaJS.$(window.getSelection().getRangeAt(0).commonAncestorContainer);
+		else if (document.selection)
+			return BetaJS.$(document.selection.createRange().parentElement());
+		return null;
+	},
+	
+	selectionOffset: function () {
+		if (window.getSelection)
+			return window.getSelection().getRangeAt(0).endOffset;
+		else if (document.selection)
+			return document.selection.createRange().endOffset;
+		return null;
+	},
+	
+	selectionStart : function() {
+		if (window.getSelection)
+			return BetaJS.$(window.getSelection().getRangeAt(0).startContainer);
+		else if (document.selection)
+			return BetaJS.$(document.selection.createRange().startContainer);
 		return null;
 	},
 
-	parentElement : function(current) {
-		return "parentNode" in current ? current.parentNode : current.parentElement();
+	selectionEnd : function() {
+		if (window.getSelection)
+			return BetaJS.$(window.getSelection().getRangeAt(0).endContainer);
+		else if (document.selection)
+			return BetaJS.$(document.selection.createRange().endContainer);
+		return null;
 	},
 
-	elementInScope : function(element, ancestor, including) {
-		including = BetaJS.Types.is_defined(including) ? including : true;
-		return element == ancestor ? including : (element == null ? false : this.elementInScope(this.parentElement(element), ancestor, including));
-	},
-
-	parentElements : function(element, ancestor, including) {
+	selectionNodes: function () {
 		var result = [];
-		var current = element;
-		while (current != null && (!ancestor || current != ancestor)) {
+		var start = this.selectionStart();
+		var end = this.selectionEnd();
+		result.push(start);
+		var current = start;
+		while (current.get(0) != end.get(0)) {
+			current = this.traverseNext(current);
 			result.push(current);
-			current = this.parentElement(current);
 		}
-		if (current == null && ancestor)
-			return [];
-		if (ancestor && including)
-			result.push(ancestor);
 		return result;
 	},
-
-	hasParentElementsTag : function(tag, element, ancestor, including) {
-		var elements = this.parentElements(element, ancestor, including);
-		tag = tag.toLowerCase();
-		return BetaJS.Objs.exists(elements, function(parent) {
-			return parent.tagName.toLowerCase() == tag;
-		});
+	
+	selectionLeaves: function () {
+		return BetaJS.Objs.filter(this.selectionNodes(), function (node) { return node.children().length == 0; });
 	}
+		
 };
 
 BetaJS.Browser = BetaJS.Browser || {};
