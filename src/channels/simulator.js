f9ad722738b77b390067efb0e6147afc06739cb6
Oliver Friedmann <oliver.friedmann@gmail.com>
2016-06-28 13:57:48 -0400

    Reference SimulatorSender Channel

--- src/channels/simulator.js
+++ src/channels/simulator.js
@@ -1,9 +1,29 @@
-Scoped.define("module:Channels.SimulatorSender", ["module:Channels.Sender"], function (Sender, scoped) {
+Scoped.define("module:Channels.SimulatorSender", [
+    "module:Channels.Sender"
+], function (Sender, scoped) {
 	return Sender.extend({scoped: scoped}, function (inherited) {
+		
+		/**
+		 * Sender Simulating Online / Offline behavior
+		 * 
+		 * @class BetaJS.Channels.SimulatorSender
+		 */
 		return {
 			
+			/**
+			 * Attribute for setting / online offline
+			 * 
+			 * @member {boolean} online online / offline setting
+			 */
 			online: true,
 
+			/**
+			 * Create a new instance given an inner sender channel.
+			 * 
+			 * @param {object} sender sender instance
+			 * 
+			 * @return {object} simulated sender instance
+			 */
 			constructor: function (sender) {
 				inherited.constructor.call(this);
 				this.__sender = sender;
