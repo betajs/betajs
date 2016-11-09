829ac57511fa699f8dfef80d3ba3d3b70c473f90
Oliver Friedmann <oliver.friedmann@gmail.com>
2016-11-08 19:02:58 -0500

    References

--- src/states/states.js
+++ src/states/states.js
@@ -1,14 +1,20 @@
 Scoped.define("module:States.Host", [
-                                     "module:Properties.Properties",
-                                     "module:Events.EventsMixin",
-                                     "module:States.State",
-                                     "module:Types",
-                                     "module:Strings",
-                                     "module:Classes.ClassRegistry"
-                                     ], function (Class, EventsMixin, State, Types, Strings, ClassRegistry, scoped) {
+    "module:Properties.Properties", "module:Events.EventsMixin", "module:States.State", "module:Types", "module:Strings", "module:Classes.ClassRegistry"
+], function (Class, EventsMixin, State, Types, Strings, ClassRegistry, scoped) {
 	return Class.extend({scoped: scoped}, [EventsMixin, function (inherited) {
+		
+		/**
+		 * State Machine Host Class
+		 * 
+		 * @class BetaJS.States.Host
+		 */
 		return {
 
+			/**
+			 * Creates a new instance.
+			 * 
+			 * @param {object} options options for state host
+			 */
 			constructor: function (options) {
 				inherited.constructor.call(this);
 				options = options || {};
@@ -17,6 +23,13 @@ Scoped.define("module:States.Host", [
 				this._enabled = true;
 			},
 
+			/**
+			 * Initialize state machine.
+			 * 
+			 * @param initial_state initial state as string or class
+			 * @param {object} initial_args initial arguments for state
+			 * 
+			 */
 			initialize: function (initial_state, initial_args) {
 				if (!this._stateRegistry) {
 					var s = null;
@@ -31,85 +44,199 @@ Scoped.define("module:States.Host", [
 					this._stateRegistry = this._auto_destroy(new ClassRegistry(Scoped.getGlobal(s)));
 				}
 				this._createState(initial_state, initial_args).start();
-				this._baseState = this._baseState || this._state.cls; 
+				this._baseState = this._baseState || this._state.cls;
+				return this;
 			},
 
+			/**
+			 * Creates a new state.
+			 * 
+			 * @protected
+			 * 
+			 * @param state state as string or class
+			 * @param {object} args arguments for state
+			 * @param {object} transitionals transitional arguments for state
+			 * 
+			 * @return {object} created state
+			 */
 			_createState: function (state, args, transitionals) {
 				return this._stateRegistry.create(state, this, args || {}, transitionals || {});
 			},
 
+			/**
+			 * Finalize current state.
+			 */
 			finalize: function () {
 				if (this._state)
 					this._state.end();
-				this._state = null;    	
+				this._state = null;
+				return this;
 			},
 
+			/**
+			 * @override
+			 */
 			destroy: function () {
 				this.finalize();
 				inherited.destroy.call(this);
 			},
 			
+			/**
+			 * Enable the state machine.
+			 */
 			enable: function () {
 				this._enabled = true;
+				return this;
 			},
 			
+			/**
+			 * Disable the state machine.
+			 */
 			disable: function () {
 				this._enabled = false;
+				return this;
 			},
 
+			/**
+			 * Returns the current state.
+			 * 
+			 * @return {object} current state
+			 */
 			state: function () {
 				return this._state;
 			},
 
+			/**
+			 * Returns the current state name.
+			 * 
+			 * @return {string} state name
+			 */
 			state_name: function () {
 				return this.state().state_name();
 			},
 
+			/**
+			 * Transitions to the next state
+			 * 
+			 * @return {object} next state
+			 */
 			next: function () {
 				return this.state() ? this.state().next.apply(this.state(), arguments) : this.initialize.apply(this, arguments);
 			},
 			
+			/**
+			 * Weakly transitions to the next state
+			 * 
+			 * @return {object} next state
+			 */
 			weakNext: function () {
 				return this.state() ? this.state().weakNext.apply(this.state(), arguments) : this.initialize.apply(this, arguments);
 			},
 			
+			/**
+			 * Starts a new state.
+			 * 
+			 * @protected
+			 * 
+			 * @param {object} state state to start
+			 */
 			_start: function (state) {
 				this._stateEvent(state, "before_start");
 				this._state = state;
 				this.set("name", state.state_name());
 			},
 
+			/**
+			 * Called after an event was started.
+			 * 
+			 * @protected
+			 * 
+			 * @param {object} state state in question
+			 */
 			_afterStart: function (state) {
 				this._stateEvent(state, "start");
 			},
 
+			/**
+			 * End a state.
+			 * 
+			 * @protected
+			 * 
+			 * @param {object} state state in question
+			 */
 			_end: function (state) {
 				this._stateEvent(state, "end");
 				this._state = null;
 			},
 
+			/**
+			 * Called after an event was ended.
+			 * 
+			 * @protected
+			 * 
+			 * @param {object} state state in question
+			 */
 			_afterEnd: function (state) {
 				this._stateEvent(state, "after_end");
 			},
 
+			/**
+			 * Called when a transition to a state is taking place.
+			 * 
+			 * @protected
+			 * 
+			 * @param {object} state state in question
+			 */
 			_next: function (state) {
 				this._stateEvent(state, "next");
 			},
 
+			/**
+			 * Called after transitioning to a state.
+			 * 
+			 * @protected
+			 * 
+			 * @param {object} state state in question
+			 */
 			_afterNext: function (state) {
 				this._stateEvent(state, "after_next");
 			},
 
+			/**
+			 * Determines whether we can transition to a state
+			 * 
+			 * @protected
+			 * 
+			 * @param {object} state state in question
+			 * @return {boolean} true if we can transition
+			 */
 			_can_transition_to: function (state) {
 				return this._enabled;
 			},
 
+			/**
+			 * Triggers a state event.
+			 * 
+			 * @protected
+			 * 
+			 * @param {object} state state in question
+			 * @param {string} s name of event
+			 */
 			_stateEvent: function (state, s) {
 				this.trigger("event", s, state.state_name(), state.description());
 				this.trigger(s, state.state_name(), state.description());
 				this.trigger(s + ":" + state.state_name(), state.description());
 			},
 
+			/**
+			 * Registers a new state.
+			 * 
+			 * @param {string} state_name name of new state
+			 * @param {object} parent_state class of parent state we should inherit from
+			 * @param {object} extend extension of state class
+			 * 
+			 * @return {object} new state class
+			 */
 			register: function (state_name, parent_state, extend) {
 				if (!Types.is_string(parent_state)) {
 					extend = parent_state;
@@ -131,13 +258,15 @@ Scoped.define("module:States.Host", [
 
 
 Scoped.define("module:States.State", [
-                                      "module:Class",
-                                      "module:Types",
-                                      "module:Strings",
-                                      "module:Async",
-                                      "module:Objs"
-                                      ], function (Class, Types, Strings, Async, Objs, scoped) {
+    "module:Class", "module:Types", "module:Strings", "module:Async", "module:Objs"
+], function (Class, Types, Strings, Async, Objs, scoped) {
 	return Class.extend({scoped: scoped}, function (inherited) {
+		
+		/**
+		 * Abstract State Class
+		 * 
+		 * @class BetaJS.States.State
+		 */
 		return {
 
 			_locals: [],
@@ -327,10 +456,23 @@ Scoped.define("module:States.State", [
 });
 
 
-Scoped.define("module:States.StateRouter", ["module:Class", "module:Objs"], function (Class, Objs, scoped) {
+Scoped.define("module:States.StateRouter", [
+	"module:Class", "module:Objs"
+], function (Class, Objs, scoped) {
 	return Class.extend({scoped: scoped}, function (inherited) {
+		
+		/**
+		 * State Router Class
+		 * 
+		 * @class BetaJS.States.StateRouter
+		 */
 		return {
 
+			/**
+			 * Creates a new instance.
+			 * 
+			 * @param {object} host state host
+			 */
 			constructor: function (host) {
 				inherited.constructor.call(this);
 				this._host = host;
@@ -338,18 +480,32 @@ Scoped.define("module:States.StateRouter", ["module:Class", "module:Objs"], func
 				this._states = {};
 			},
 
+			/**
+			 * Register a route.
+			 * 
+			 * @param {string} route route to be registered
+			 * @param {string} state state to be registered
+			 * @param {array} mapping optional argument mapping
+			 */
 			registerRoute: function (route, state, mapping) {
 				var descriptor = {
-						key: route,
-						route: new RegExp("^" + route + "$"),
-						state: state,
-						mapping: mapping || []
+					key: route,
+					route: new RegExp("^" + route + "$"),
+					state: state,
+					mapping: mapping || []
 				};
 				this._routes.push(descriptor);
 				this._states[state] = descriptor;
 				return this;
 			},
-
+			
+			/**
+			 * Read a route from a state object.
+			 * 
+			 * @param {object} stateObject state object
+			 * 
+			 * @return {string} corresponding route
+			 */
 			readRoute: function (stateObject) {
 				var descriptor = this._states[stateObject.state_name()];
 				if (!descriptor)
@@ -362,6 +518,13 @@ Scoped.define("module:States.StateRouter", ["module:Class", "module:Objs"], func
 				return route;
 			},
 
+			/**
+			 * Parses a route.
+			 * 
+			 * @param {string} route route to be parsed
+			 * 
+			 * @return {object} state and argument descriptor
+			 */
 			parseRoute: function (route) {
 				for (var i = 0; i < this._routes.length; ++i) {
 					var descriptor = this._routes[i];
@@ -379,14 +542,25 @@ Scoped.define("module:States.StateRouter", ["module:Class", "module:Objs"], func
 				return null;
 			},
 
+			/**
+			 * Returns the current route.
+			 * 
+			 * @return {string} current route
+			 */
 			currentRoute: function () {
 				return this.readRoute(this._host.state());
 			},
 
+			/**
+			 * Navigate to a route.
+			 * 
+			 * @param {string} route route to be navigated to
+			 */
 			navigateRoute: function (route) {
 				var parsed = this.parseRoute(route);
 				if (parsed)
 					this._host.next(parsed.state, parsed.args);
+				return this;
 			}
 
 		};		
