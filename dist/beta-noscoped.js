0b87b1fb665e5e452e4624a5d6d5eff5e034b720
Oliver Friedmann <oliver.friedmann@gmail.com>
2019-08-19 21:15:31 -0400

    Promise.mapASuccess

--- dist/beta-noscoped.js
+++ dist/beta-noscoped.js
@@ -11,7 +11,7 @@ Scoped.define("module:", function () {
 	return {
     "guid": "71366f7a-7da3-4e55-9a0b-ea0e4e2a9e79",
     "version": "1.0.189",
-    "datetime": 1566263518602
+    "datetime": 1566263713318
 };
 });
 Scoped.require(['module:'], function (mod) {
